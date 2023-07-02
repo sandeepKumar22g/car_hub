2739da7a58a8a15a1b84dc6b0059b26f8f3964cd
Sandeep <sandy6862g22@gmail.com>
2023-07-02 17:44:38 +0530

    added components from my last project

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { CustomButtonProps } from '@/types'
+import React from 'react'
+
+const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
+  return (
+    <button disabled={false} type={btnType || "button"} className={`flex flex-row relative justify-center items-center py-3 px-6 outline-none ${containerStyles}`} onClick={handleClick} >
+        <span className={`flex-1`}>
+            {title}
+        </span>
+    </button>
+  )
+}
+
+export default CustomButton
