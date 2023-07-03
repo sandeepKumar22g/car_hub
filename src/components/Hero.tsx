5434b281a6141d731c58fd2c1273dbb19884c545
Sandeep <sandy6862g22@gmail.com>
2023-07-03 12:12:14 +0530

    aaded search bar component and filter options

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
   return (
     <>
         <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
-            <div className="flex-1 pt-36 padding-x">
+            <div className="flex-1 pt-36 sm:px-16 px-6">
                 <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
                     Find, Book, or Rent a Car — quickly and easily!
                 </h1>
--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
 }
+
+export interface SearchManufacturerprops {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}
