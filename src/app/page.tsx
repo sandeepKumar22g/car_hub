0b04689359256aa381b755ee3351a15d1ee70c50
Sandeep <sandy6862g22@gmail.com>
2023-07-04 12:47:02 +0530

    added car cards components and api utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import {CustomFilter, Hero, SearchBar} from '@/components'
+import {CarCard, CustomFilter, Hero, SearchBar} from '@/components'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars()
+  console.log(allCars)
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -16,6 +20,22 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+        {
+          !isDataEmpty ? ( 
+            <section>
+              <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full gap-8 pt-14">
+                {
+                  allCars?.map(car => (<CarCard car={car} />))
+                }
+              </div>
+            </section>
+          ): (
+            <div className='mt-16 flex justify-center items-center flex-col gap-2'>
+              <h2 className='text-black text-xl font-bold'>No results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )
+        }
       </div>
     </main>
   )
--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,14 +1,20 @@
 "use client"
 
 import { CustomButtonProps } from '@/types'
+import Image from 'next/image'
 import React from 'react'
 
-const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, btnType, textStyles, rightIcon}: CustomButtonProps) => {
   return (
     <button disabled={false} type={btnType || "button"} className={`flex flex-row relative justify-center items-center py-3 px-6 outline-none ${containerStyles}`} onClick={handleClick} >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
+        {rightIcon && (
+          <div className="realtive w-6 h-6">
+            <Image src={rightIcon} alt="alt" fill className="object-contain" />
+          </div>
+        )}
     </button>
   )
 }
